2e9ae450e41ff99c1d247626f5a3eaefbbbcd341
Amin <amin.haddou@frg.wwschool.de>
2023-04-16 13:34:41 +0200

    send qr code to discord webhook

--- src/main/kotlin/de/duckulus/floppa/Manager.kt
+++ src/main/kotlin/de/duckulus/floppa/Manager.kt
@@ -2,31 +2,41 @@ package de.duckulus.floppa
 
 import de.duckulus.floppa.command.CommandManager
 import io.github.cdimascio.dotenv.Dotenv
+import io.ktor.util.*
 import it.auties.whatsapp.api.Whatsapp
 import it.auties.whatsapp.api.WhatsappOptions.WebOptions
 import it.auties.whatsapp.controller.DefaultControllerSerializer
 import it.auties.whatsapp.listener.OnLoggedIn
+import kotlinx.coroutines.runBlocking
+import java.net.URLEncoder
 import java.nio.file.Path
-import kotlin.system.exitProcess
 
 val env: Dotenv = Dotenv.configure().ignoreIfMissing().load()
 
 val JDOODLE_CLIENT_ID: String = env["JDOODLE_CLIENT_ID"]
 val JDOODLE_CLIENT_SECRET: String = env["JDOODLE_CLIENT_SECRET"]
 val OPENAI_API_KEY: String = env["OPENAI_API_KEY"]
+val DISCORD_WEBHOOK_URL: String = env["DISCORD_WEBHOOK_URL"]
 
+@OptIn(InternalAPI::class)
 fun main() {
-    if (JDOODLE_CLIENT_ID.isEmpty() || JDOODLE_CLIENT_SECRET.isEmpty() || OPENAI_API_KEY.isEmpty()) {
-        println("Missing environment variables")
-        exitProcess(1)
-    }
     CommandManager.registerCommands()
 
     val serializer = DefaultControllerSerializer(Path.of("./session"))
-    val api = Whatsapp.lastConnection(
+    val api = Whatsapp.newConnection(
         WebOptions.builder()
             .serializer(serializer)
             .deserializer(serializer)
+            .qrHandler {
+                runBlocking {
+                    val url = "https://quickchart.io/qr?text=${URLEncoder.encode(it, "UTF-8")}"
+                    println(url)
+                    sendWebhookImage(
+                        DISCORD_WEBHOOK_URL,
+                        url
+                    )
+                }
+            }
             .build()
     )
     api.addLoggedInListener(OnLoggedIn {
---
+++ src/main/kotlin/de/duckulus/floppa/util.DiscordWebhook.kt
@@ -0,0 +1,29 @@
+package de.duckulus.floppa
+
+import de.duckulus.floppa.http.ktorClient
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WebhookPayload(
+    val content: String,
+    val embeds: List<Embed>
+)
+
+@Serializable
+data class Embed(
+    val image: Image,
+)
+
+@Serializable
+data class Image(
+    val url: String
+)
+
+suspend fun sendWebhookImage(webhookUrl: String, imageUrl: String) {
+    ktorClient.post(webhookUrl) {
+        contentType(ContentType.Application.Json)
+        setBody(WebhookPayload("", listOf(Embed(Image(imageUrl)))))
+    }
+}
