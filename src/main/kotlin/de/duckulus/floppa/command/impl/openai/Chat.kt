8a64b303657760c6b33158bce85f5bf54e688200
Amin <amin.haddou@frg.wwschool.de>
2023-05-05 20:06:22 +0200

    add per-chat context to chat command

--- src/main/kotlin/de/duckulus/floppa/command/impl/openai/Chat.kt
+++ src/main/kotlin/de/duckulus/floppa/command/impl/openai/Chat.kt
@@ -10,10 +10,21 @@ import de.duckulus.floppa.command.CommandContext
 import de.duckulus.floppa.command.PermissionLevel
 import it.auties.whatsapp.api.Emojy
 import it.auties.whatsapp.api.Whatsapp
+import it.auties.whatsapp.model.contact.ContactJid
 import it.auties.whatsapp.model.info.MessageInfo
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.util.concurrent.ArrayBlockingQueue
+
+const val contextSize = 10
 
 object Chat : Command("chat", "generates text with gpt-3.5-turbo", PermissionLevel.ADMIN) {
+
+    private val mutex = Mutex()
+
+    @OptIn(BetaOpenAI::class)
+    private val contexts = mutableMapOf<ContactJid, ArrayBlockingQueue<ChatMessage>>()
 
     @OptIn(BetaOpenAI::class)
     override fun execute(whatsapp: Whatsapp, messageInfo: MessageInfo, args: Array<String>, ctx: CommandContext) {
@@ -28,13 +39,48 @@ object Chat : Command("chat", "generates text with gpt-3.5-turbo", PermissionLev
                 return@runBlocking
             }
 
+            val contextList: MutableList<ChatMessage>
+
+            mutex.withLock {
+                val context = if (contexts[messageInfo.chatJid()] == null) {
+                    ArrayBlockingQueue<ChatMessage>(contextSize)
+                } else {
+                    contexts[messageInfo.chatJid()]!!
+                }
+
+                contextList = context.toMutableList()
+                contextList.add(ChatMessage(ChatRole.User, prompt))
+                println(contextList.toTypedArray().contentToString())
+            }
+
+
             val reply = openAi.chatCompletion(
                 ChatCompletionRequest(
-                    messages = listOf(ChatMessage(ChatRole.User, prompt)),
+                    messages = contextList,
                     model = ModelId("gpt-3.5-turbo")
                 )
             )
             whatsapp.sendMessage(messageInfo.chat(), reply.choices.first().message?.content ?: "No response :(")
+
+            mutex.withLock {
+                val context = if (contexts[messageInfo.chatJid()] == null) {
+                    ArrayBlockingQueue<ChatMessage>(contextSize)
+                } else {
+                    contexts[messageInfo.chatJid()]!!
+                }
+
+                if (!context.offer(ChatMessage(ChatRole.User, prompt))) {
+                    context.poll()
+                    context.offer(ChatMessage(ChatRole.User, prompt))
+                }
+
+                if (!context.offer(ChatMessage(ChatRole.Assistant, reply.choices.first().message?.content ?: ""))) {
+                    context.poll()
+                    context.offer(ChatMessage(ChatRole.Assistant, reply.choices.first().message?.content ?: ""))
+                }
+
+                contexts[messageInfo.chatJid()] = context
+            }
         }
 
     }
